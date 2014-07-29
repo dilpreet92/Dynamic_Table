4ba11e8e2454aff050ba377c92fb33dd828b4520
Dilpreet Singh <dilpreet@vinsol.com>
2014-07-29 12:22:49 +0530

    Changing the files as discussed

--- dynamic_table.js
+++ dynamic_table.js
@@ -3,67 +3,59 @@ function CreateDynamicTable(elements) {
   this.tableElement = elements.tableElement;
 }
 
-CreateDynamicTable.prototype.EMAIL_STR = "([a-z]|\d|.|#|$|)*@([a-z]|\d)*.([a-z][a-z][a-z]|[a-z][a-z].[a-z][a-z])";
+CreateDynamicTable.prototype.EMAIL_STR = "([a-z]|[0-9]|.)+@([a-z]|[0-9])+.([a-z]{3,}|[a-z]{3,}.[a-z]{2})";
  
 CreateDynamicTable.prototype.NAME_STR = "([a-zA-Z][a-zA-Z]*)";  
 
-CreateDynamicTable.prototype.addNewRow = function() {
-  var saveButton = $("<button />").text("save").addClass("save"),
-      nameControl = $("<input/>").addClass("name"),
-      emailControl = $("<input/>").addClass("email");
-  this.tableElement.find("tr:last").after($("<tr/>")
-    .append($("<td/>").append(nameControl))
-    .append($("<td/>").append(emailControl))
-    .append($("<td/>").append(saveButton))); 
+CreateDynamicTable.prototype.addInputTextBox = function(classOfInputElement) {
+  return ($("<input/>").addClass(classOfInputElement));
 };
 
-CreateDynamicTable.prototype.getRowElements = function(currentElement,classOfElement) {
+CreateDynamicTable.prototype.addButton = function(value , classOfElement) {
+  return ($("<button />").text(value).addClass(classOfElement));
+};
+
+CreateDynamicTable.prototype.addNewRow = function() {
+  this.tableElement.find("tr:last").after($("<tr/>")
+    .append(
+      $("<td/>").append(this.addInputTextBox("name")),
+      $("<td/>").append(this.addInputTextBox("email")),
+      $("<td/>").append(this.addButton("save", "save"))
+      )); 
+};
+
+CreateDynamicTable.prototype.getRowElements = function(currentElement, classOfElement) {
    return (currentElement.closest("tr").find(classOfElement));
 };
 
-CreateDynamicTable.prototype.isNameValid = function(currentName) {
-  var NAME_PATT = new RegExp(this.NAME_STR);
-  if(!NAME_PATT.test(currentName)) {
-    alert("Please Enter Name");
-    return false;
-  }
-  return true;
-};
-
-CreateDynamicTable.prototype.isEmailValid = function(currentEmail) {
-  var EMAIL_PATT = new RegExp(this.EMAIL_STR);
-  if(!EMAIL_PATT.test(currentEmail)) {
-    alert("Please Enter Correct Email");
+CreateDynamicTable.prototype.validate = function(currentItemValue, currentString) {
+  var PATT = new RegExp(currentString);
+  if(!PATT.test(currentItemValue)) {
+    alert("Please Enter Correct Details");
     return false;
   }
   return true;
 };
 
 CreateDynamicTable.prototype.editText = function(cuurentEditButton) {
-  var nameTextElement = this.getRowElements(cuurentEditButton,".name"),
-      emailTextElement = this.getRowElements(cuurentEditButton,".email");
-  this.addTextBox(nameTextElement,"name"); 
-  this.addTextBox(emailTextElement,"email");
+  var nameTextElement = this.getRowElements(cuurentEditButton, ".name"),
+      emailTextElement = this.getRowElements(cuurentEditButton, ".email");
+  this.replaceLabel(nameTextElement,"name"); 
+  this.replaceLabel(emailTextElement,"email");
   this.enableSaveButton(cuurentEditButton);   
 };
 
 CreateDynamicTable.prototype.enableSaveButton = function(cuurentEditButton) {
-  cuurentEditButton.parent("td").replaceWith($("<td/>").append($("<button />").text("save").addClass("save")));
+  cuurentEditButton.parent("td").replaceWith($("<td/>").append(this.addButton("save", "save")));
 };
 
-CreateDynamicTable.prototype.addTextBox = function(currentElement,classOfElement) {
-  var inputElement = $("<input/>");
-  currentElement.replaceWith(inputElement.val(currentElement.text()).addClass(classOfElement));
+CreateDynamicTable.prototype.replaceLabel = function(currentElement,classOfElement) {
+  currentElement.replaceWith(this.addInputTextBox(classOfElement).attr("value", currentElement.text()));
 };
 
-CreateDynamicTable.prototype.deleteRow = function(currentDeleteButton) {
-  currentDeleteButton.closest("tr").remove();
-};
 
-CreateDynamicTable.prototype.enableEditDelete = function(currentSaveButton) {
-  var editButton = $("<button />").text("Edit").addClass("edit"),
-      deleteButton = $("<button />").text("Delete").addClass("delete");    
-  currentSaveButton.replaceWith(editButton,deleteButton);
+CreateDynamicTable.prototype.enableEditDelete = function(currentSaveButton) {  
+  currentSaveButton.replaceWith(this.addButton("Edit", "edit"),this.addButton("Delete", "delete"));
 };
 
 CreateDynamicTable.prototype.addLabel = function(currentElement,classOfElement) {
@@ -74,10 +66,10 @@ CreateDynamicTable.prototype.addLabel = function(currentElement,classOfElement) 
 CreateDynamicTable.prototype.saveText = function(currentSaveButton) {
   var emailTextElement = this.getRowElements(currentSaveButton,".email"),
       nameTextElement = this.getRowElements(currentSaveButton,".name");
-  if(this.isNameValid(nameTextElement.val()) & this.isEmailValid(emailTextElement.val())) {
+  if(this.validate(nameTextElement.val(), this.NAME_STR) & this.validate(emailTextElement.val(), this.EMAIL_STR)) {
     this.enableEditDelete(currentSaveButton);
-    this.addLabel(nameTextElement,"name");
-    this.addLabel(emailTextElement,"email");    
+    this.addLabel(nameTextElement, "name");
+    this.addLabel(emailTextElement, "email");    
   }     
 };
 
@@ -93,7 +85,7 @@ CreateDynamicTable.prototype.bindEvents = function() {
     _this.editText($(this));
   });
   this.tableElement.on("click",".delete",function() {
-    _this.deleteRow($(this));
+    $(this).closest("tr").remove();
   });
 };
 
