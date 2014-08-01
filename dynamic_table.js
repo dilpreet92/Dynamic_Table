bb08dc5800873a4c816fafa10e3dbe61e309f1ae
Dilpreet Singh <dilpreet@vinsol.com>
2014-08-01 13:19:50 +0530

    Changing the RegEx

--- dynamic_table.js
+++ dynamic_table.js
@@ -1,11 +1,9 @@
-function CreateDynamicTable(elements) {
+function CreateDynamicTable(elements, regex) {
   this.buttonElement = elements.buttonElement;
   this.tableElement = elements.tableElement;
+  this.emailRegExp = regex.email;
+  this.nameRegExp = regex.name;
 }
-
-CreateDynamicTable.prototype.EMAIL_STR = "([a-z]|[0-9]|.)+@([a-z]|[0-9])+.([a-z]{3,}|[a-z]{3,}.[a-z]{2})";
- 
-CreateDynamicTable.prototype.NAME_STR = "([a-zA-Z][a-zA-Z]*)";  
 
 CreateDynamicTable.prototype.addInputTextBox = function(classOfInputElement) {
   return ($("<input/>").addClass(classOfInputElement));
@@ -28,10 +26,9 @@ CreateDynamicTable.prototype.getRowElements = function(currentElement, classOfEl
    return (currentElement.closest("tr").find(classOfElement));
 };
 
-CreateDynamicTable.prototype.validate = function(currentItemValue, currentString) {
-  var PATT = new RegExp(currentString);
-  if(!PATT.test(currentItemValue)) {
-    alert("Please Enter Correct Details");
+CreateDynamicTable.prototype.validate = function(currentItemValue, currentString, errorString) {
+  if(!currentString.test(currentItemValue)) {
+    alert("Please Enter Correct " + errorString);
     return false;
   }
   return true;
@@ -66,7 +63,7 @@ CreateDynamicTable.prototype.addLabel = function(currentElement,classOfElement) 
 CreateDynamicTable.prototype.saveText = function(currentSaveButton) {
   var emailTextElement = this.getRowElements(currentSaveButton,".email"),
       nameTextElement = this.getRowElements(currentSaveButton,".name");
-  if(this.validate(nameTextElement.val(), this.NAME_STR) & this.validate(emailTextElement.val(), this.EMAIL_STR)) {
+  if(this.validate(nameTextElement.val(), this.nameRegExp, "name") & this.validate(emailTextElement.val(), this.emailRegExp, "Email")) {
     this.enableEditDelete(currentSaveButton);
     this.addLabel(nameTextElement, "name");
     this.addLabel(emailTextElement, "email");    
@@ -89,11 +86,16 @@ CreateDynamicTable.prototype.bindEvents = function() {
   });
 };
 
+ 
 $(document).ready(function() {
   var elements = {
     "buttonElement" : $("#addRow"),
     "tableElement" : $("#userData")
   };
-  var addRowObj = new CreateDynamicTable(elements);
+  var regex = {
+    "email" : /^\w+@(\w)+\.(\w{3}|\w{2}\.\w{2}){1}$/i,
+    "name" : /^[a-z]+$/i
+  };
+  var addRowObj = new CreateDynamicTable(elements, regex);
   addRowObj.bindEvents();
 });
